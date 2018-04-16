39ba72c8151b86c7625a7171fbc7e72f0d8d9e50
jangerhard <jgschoepp@gmail.com>
2018-04-16 22:47:35 +0200

    Include GraphQL call and console log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,89 @@
 import React from 'react';
 
+const API = 'https://api.github.com/graphql';
+const API_KEY = 'x';
+
 class GithubShowcase extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch(API, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "bearer " + API_KEY
+            },
+            body: JSON.stringify({ query: query }),
+        })
+            .then(res => res.json())
+            .then(res => console.log(res));
+    }
+
     render() {
         return (
-            <div>Hey Cathy!</div>
+            <div>{}</div>
     );
     }
 }
 export default GithubShowcase;
+
+export const query = `
+  query GithubQuery {
+
+  user(login: "jangerhard") {
+
+    repositories(first: 3, orderBy: {field: UPDATED_AT, direction: DESC}) {
+
+      edges {
+
+        node {
+
+          name
+
+          ref(qualifiedName: "master"){
+
+            target {
+
+              ... on Commit {
+
+                history (first: 2) {
+
+                  nodes{
+
+                    author {
+
+                      name
+
+                      avatarUrl
+
+                    }
+
+                    message
+
+                  }
+
+                }
+
+              }
+
+            }
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+}
+`;
