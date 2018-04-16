28a82c78dda94b7ca383ec7f870189cb6fb0237b
jangerhard <jgschoepp@gmail.com>
2018-04-16 21:05:40 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class GithubShowcase extends React.Component {
+    render() {
+        return (
+            <div>Hey Cathy!</div>
+    );
+    }
+}
+export default GithubShowcase;
