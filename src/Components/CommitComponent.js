3eac9c22b3ea272cae8f010ba6d0f40f0ae5ce58
jangerhard <jgschoepp@gmail.com>
2018-04-27 21:44:25 +0200

    Add Moment.js and format commit message

--- src/Components/CommitComponent.js
+++ src/Components/CommitComponent.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 class CommitComponent extends React.Component {
 
     render() {
 
+        const date = moment(this.props.committedDate).fromNow();
+
         return(
             <div style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "left",
                 paddingLeft: "1em"
             }}>
-                {this.props.abbreviatedOid}: {this.props.message} ({this.props.committedDate})
+                <div style={{
+                    paddingLeft: "1em"
+                }}>{this.props.abbreviatedOid}</div>
+                :
+                <div style={{
+                    paddingLeft: "0.5em"
+                }}>
+                    {this.props.message}
+                    </div>
+                <div style={{
+                    
+                    paddingLeft: "0.5em"
+                }}> </div>
+                 ({date})
             </div>
         )
     }
