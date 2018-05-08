42d46319c665c62b22f31379df7bd14821428da9
jangerhard <jgschoepp@gmail.com>
2018-05-08 08:32:36 +0200

    Add ProfileInfo test

---
+++ src/Components/__tests__/ProfileInfoComponent.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ShallowRenderer from 'react-test-renderer/shallow';
+import ProfileInfoComponent from "../ProfileInfoComponent";
+
+const fullName = 'Test Testington';
+const avatarUrl = 'www.avatarurl.com';
+
+it('renders profile-info correctly', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(
+        <ProfileInfoComponent
+            fullName={fullName}
+            avatarUrl={avatarUrl}
+        />
+    );
+    const tree = renderer.getRenderOutput();
+    expect(tree).toMatchSnapshot();
+});
