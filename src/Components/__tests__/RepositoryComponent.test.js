04e538666b119893c15871e19b5dd898ee33d8e3
jangerhard <jgschoepp@gmail.com>
2018-05-08 08:32:57 +0200

    Add Repository test

---
+++ src/Components/__tests__/RepositoryComponent.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ShallowRenderer from 'react-test-renderer/shallow';
+import RepositoryComponent from "../RepositoryComponent";
+
+const name = 'Test Testington';
+const commits = [
+    {
+        abbreviatedOid: 'AAAAAA',
+        message: 'This is a commit message',
+        committedDate: '2018-05-04T19:59:54Z',
+    }
+];
+const url = 'www.test.com';
+
+test('renders repository correctly', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(
+        <RepositoryComponent
+            commits={commits}
+            name={name}
+            url={url}
+        />
+    );
+    const tree = renderer.getRenderOutput();
+    expect(tree).toMatchSnapshot();
+});
