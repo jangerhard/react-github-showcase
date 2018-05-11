4f28c1423543d701d1508644d8e088f76e17df69
Jan Gerhard Schoepp <jgschoepp@gmail.com>
2018-05-11 12:53:31 +0200

    Feature - Darken Commit messages (#17)
    * Darken commit-date as mentioned in issue #5

--- src/Components/CommitComponent.js
+++ src/Components/CommitComponent.js
@@ -2,13 +2,26 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const commitStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: "1em"
+};
+
+const dateStyle = {
+    opacity: ".5",
+    paddingLeft: "0.5em"
+};
+
 const CommitComponent = ({committedDate, abbreviatedOid, message}) => {
 
     const date = moment(committedDate).fromNow();
 
     return (
-        <div style={{paddingLeft: "1em"}}>
-            {abbreviatedOid}: {message} ({date})
+        <div style={commitStyle}>
+            {abbreviatedOid}: {message}
+            <div style={dateStyle}>{date}</div>
         </div>
     )
 };
