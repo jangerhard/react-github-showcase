07be61920a1b4f58a673dc1185f2aa71b7159c87
Jan Gerhard Schoepp <jgschoepp@gmail.com>
2018-05-11 21:41:02 +0200

    Bug handle no commits (#19)
    * Update repositories component to be able to handle no commits
    
    * Update repository component to be able to handle no commits

--- src/Components/RepositoryComponent.js
+++ src/Components/RepositoryComponent.js
@@ -5,7 +5,8 @@ import CommitComponent from "./CommitComponent";
 const RepositoryComponent = ({ name, commits, url }) => (
     <div>
         <a href={url}> {name} </a>
-        {commits.map(function (commit) {
+        {!commits.length && <NoCommitMessage/>}
+        {commits.map((commit) => {
             return <CommitComponent
                 key={commit.abbreviatedOid}
                 abbreviatedOid={commit.abbreviatedOid}
@@ -20,5 +21,15 @@ RepositoryComponent.propTypes = {
     commits: PropTypes.array.isRequired,
     url: PropTypes.string.isRequired
 };
+
+const noCommitMessageStyle = {
+    paddingLeft: "1em"
+};
+
+const NoCommitMessage = () => (
+    <div style={noCommitMessageStyle}>
+        No commits yet.
+    </div>
+);
 
 export default RepositoryComponent;
